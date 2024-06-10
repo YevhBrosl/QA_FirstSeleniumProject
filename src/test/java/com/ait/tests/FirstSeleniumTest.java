c90607da415a21057c4056cba9cf63a8a2827e36
Yevheniia <yevheniia79@gmail.com>
2024-06-10 21:04:47 +0200

    first commit

---
+++ src/test/java/com/ait/tests/FirstSeleniumTest.java
@@ -0,0 +1,38 @@
+package com.ait.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    //before-setUp
+    @BeforeMethod
+    public void setUo() {
+        driver = new ChromeDriver();
+        //driver.get("https://www.google.com");
+
+        driver.navigate().to("https://www.google.com"); // opens with history
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    //tests
+    @Test
+    public void openGoogle() {
+        System.out.println("Google opened!");
+    }
+
+    //after-tearDown
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        // interview question!!!
+        driver.quit(); // closes all tabs && browser
+        //driver.close(); // closes current tab
+    }
+}
